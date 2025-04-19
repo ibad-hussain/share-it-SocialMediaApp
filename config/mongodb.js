97090b77fb01da814a9026eb8cc0adc44c7b3f4c
ibad-hussain <ibadhussain2212@gmail.com>
2025-04-19 16:42:17 +0500

    Update configuration files

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
 
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/share-it`);
+        await mongoose.connect(`${process.env.MONGODB_URI}`);
         console.log("Database connected successfully")  //---------------------
     } catch (error) {
         console.error("Database connection failed:", error);
